99a75c8856f7c6041160a019d07697823ff5b8c9
Brandon McAnsh <brandon.mcansh@gmail.com>
2020-05-16 18:50:53 -0400

    chore(sample): kill filewatcher from sample
    simple record/stop recording sample
    
    Signed-off-by: Brandon McAnsh <brandon.mcansh@gmail.com>

--- app/src/main/java/dev/bmcreations/scrcast/app/list/MainActivity.kt
+++ app/src/main/java/dev/bmcreations/scrcast/app/list/MainActivity.kt
@@ -21,10 +21,6 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private val watcher: FileWatcher? by lazy {
-        recorder.outputDirectory?.watch(::fileWatcher)
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -36,22 +32,6 @@ class MainActivity : AppCompatActivity() {
                 recorder.record()
             }
         }
-    }
-
-    override fun onResume() {
-        super.onResume()
-        if (watcher?.isWatching == false) {
-            watcher?.watch(::fileWatcher)
-        }
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        watcher?.close()
-    }
-
-    private fun fileWatcher(files: List<File>) {
-        Log.d("Main", "files=${files.count()}")
     }
 
     private fun FloatingActionButton.reflectState(recording: Boolean) {
