306be798a5721c862c355d03769f2974d1759993
Brandon McAnsh <brandon.mcansh@gmail.com>
2020-05-18 23:10:09 -0400

    feat(sample): configure notification options
    Signed-off-by: Brandon McAnsh <brandon.mcansh@gmail.com>

--- app/src/main/java/dev/bmcreations/scrcast/app/list/MainActivity.kt
+++ app/src/main/java/dev/bmcreations/scrcast/app/list/MainActivity.kt
@@ -3,10 +3,12 @@ package dev.bmcreations.scrcast.app.list
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.graphics.drawable.toBitmap
 import dev.bmcreations.scrcast.ScrCast
 import dev.bmcreations.scrcast.app.R
+import dev.bmcreations.scrcast.config.ChannelConfig
+import dev.bmcreations.scrcast.config.NotificationConfig
 import dev.bmcreations.scrcast.config.StorageConfig
-import dev.bmcreations.scrcast.config.VideoConfig
 import kotlinx.android.synthetic.main.activity_main.*
 
 
@@ -18,6 +20,17 @@ class MainActivity : AppCompatActivity() {
                 copy(
                     storage = StorageConfig(
                         directoryName = "scrcast-sample"
+                    ),
+                    notification = NotificationConfig(
+                        title = "Super cool library",
+                        description = "shh session in progress",
+                        icon = resources.getDrawable(R.drawable.ic_camcorder, null).toBitmap(),
+                        channel = ChannelConfig(
+                            id = "1337",
+                            name = "Recording Service"
+                        ),
+                        showStop = true,
+                        showTimer = true
                     ),
                     moveTaskToBack = false
                 )
