634da5023f987d97392e8467796124a4a99cb65f
Brandon McAnsh <brandon.mcansh@gmail.com>
2020-08-01 09:45:22 -0400

    feat: build lifecycle docs
    Signed-off-by: Brandon McAnsh <brandon.mcansh@gmail.com>

--- deploy-docs.sh
+++ deploy-docs.sh
@@ -4,7 +4,7 @@
 rm -rf docs/api
 
 # Build the Dokka docs.
-./gradlew clean :library:dokka
+./gradlew clean :library:dokka :lifecycle:dokka
 
 # Copy outside files into the docs folder.
 sed -e '/full configuration details and documentation here/ { N; d; }' < README.md > docs/index.md
